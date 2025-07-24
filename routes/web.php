6e872e7b8e25eb9f5b841760ee6feee932c65ecf
x666dbg <fvckirmiskin+1@gmail.com>
2025-07-24 17:53:13 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -4,43 +4,36 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ThreadController;
 use App\Http\Controllers\ReplyController;
-use App\Http\Controllers\UserProfileController; // <-- Pastikan ini ada
+use App\Http\Controllers\UserProfileController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Arahkan halaman utama ke login
+// 1. Route utama, arahkan ke login
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-// Route untuk timeline (dashboard)
+// 2. Route dashboard, sangat spesifik
 Route::get('/dashboard', [ThreadController::class, 'index'])
     ->middleware(['auth', 'verified'])->name('dashboard');
 
+// 3. Masukkan semua route dari Breeze (login, register, dll) SEBELUM route rakus
+require __DIR__.'/auth.php';
+
 Route::middleware('auth')->group(function () {
-    // Routes untuk Threads & Replies
+    // 4. Routes spesifik lainnya (threads, replies, edit profil)
     Route::post('/threads', [ThreadController::class, 'store'])->name('threads.store');
     Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
     Route::post('/threads/{thread}/replies', [ReplyController::class, 'store'])->name('replies.store');
 
-    // Routes untuk PROFIL PUBLIK (yang kita buat)
-    Route::get('/{user:username}', [UserProfileController::class, 'show'])->name('profile.show');
-    Route::post('/{user:username}/follow', [UserProfileController::class, 'follow'])->name('profile.follow');
-    Route::post('/{user:username}/unfollow', [UserProfileController::class, 'unfollow'])->name('profile.unfollow');
-
-    // Routes untuk PENGATURAN PROFIL (bawaan Breeze)
+    // Route untuk PENGATURAN PROFIL (bawaan Breeze).
+    // Ini harus ada SEBELUM route profil publik.
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+
+    // 5. Terakhir, letakkan route yang "rakus" di bagian PALING BAWAH
+    // Ini adalah route untuk PROFIL PUBLIK.
+    Route::get('/{user:username}', [UserProfileController::class, 'show'])->name('profile.show');
+    Route::post('/{user:username}/follow', [UserProfileController::class, 'follow'])->name('profile.follow');
+    Route::post('/{user:username}/unfollow', [UserProfileController::class, 'unfollow'])->name('profile.unfollow');
+});
